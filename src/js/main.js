19f9d5d6de8cd8a1b80fa9363cd7c6337895038f
Robin Rutten <robinrutten@gmail.com>
2015-11-02 16:32:36 +0100

    added idea and floor

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 var Main = function() {
-	var avatar = new Avatar('Me', 'male', '#000', '#fff', '#0AA');
-	$('#avatar').html(avatar.render());
+	var credit = new Credit();
+	var avatar = new Avatar('Me', 'female', 'red', '#fff', '#0AA');
+	$('#avatar-svg').html(avatar.render());
+
+
+	window.showBubble = function(text) {
+		var $idea = $('.idea');
+		$('.idea-text', $idea).html(text);
+		$idea.addClass('visible');
+	}
 };

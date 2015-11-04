442161fae45ce188847af4379b90fb2298e4b2eb
Robin Rutten <robinrutten@gmail.com>
2015-11-04 15:13:52 +0100

    added dist grunt method to create dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,57 @@ module.exports = function(grunt) {
 		dist: 'dist',
 		bower_components: 'bower_components',
 
+	    copy: {
+	      dist: {
+	        files: [{
+	          expand: true,
+	          dot: true,
+	          cwd: '<%= src %>',
+	          dest: '<%= dist %>',
+	          src: [
+	            '*.{ico,png,txt}',
+	            '.htaccess', 
+	            '*.html',
+	        	'styles/{,*/}*.css', 
+	            'img/{,*/}*.{webp,png,jpg,gif,svg}', 
+	            'fonts/{,*/}*.*'
+	          ]
+	        }]
+	      }
+	    },
+
 	    sass: {
 	      main: {
 	        src: ['<%= src %>/styles/main.scss'],
 	        dest: '<%= dist %>/styles/main.css'
 	      }
 	    },
+
+	    usemin: {
+	      html: ['<%= dist %>/{,*/}*.html'],
+	      options: {
+	        assetsDirs: [
+	          '<%= dist %>',
+	          '<%= dist %>/img',
+	          '<%= dist %>/styles'
+	        ]
+	      }
+	    },
+
+		useminPrepare: {
+		  options: {
+		    dest: '<%= dist %>',
+		    flow: {
+		      html: {
+		        steps: {
+		          js: ['concat'],
+		        },
+		        post: {}
+		      }
+		    }
+		  },
+		   html: '<%= src %>/index.html',
+		},
 
 		watch: {
 		  sass: {
@@ -62,11 +107,13 @@ module.exports = function(grunt) {
         }
 
 	});
-
+	grunt.loadNpmTasks('grunt-usemin');
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.registerTask('serve', ['connect:server']);
 	grunt.registerTask('default', []);
+	grunt.registerTask('dist', ['useminPrepare', 'sass', 'copy', 'usemin', 'concat']);
 };
